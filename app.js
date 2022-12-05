15bb3cd55c7a1d184f9c0c44cbfff328b38229d7
BC Kuo <kids246@gmail.com>
2022-12-05 01:36:49 -0500

    [refactor] revise the table structure

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+// https://stackoverflow.com/questions/3244361/
+import { interests_activitys } from './interests_activities.js'
+
 // senior-activity data
 const seionr_img_src = "http://holoboro1031.github.io/senior-activity_database/[Pexels][Edu Carvalho] selective-focus-photography-of-woman-standing-near-green-plant-2050976.jpg"
-const activity = "Activity: Drink Coffe"
+const activity = "Gardening"
 
 // child info components
 const child_info_form = document.getElementById("child-info-form");
@@ -8,14 +11,27 @@ const child_info_form = document.getElementById("child-info-form");
 const child_name_text = document.getElementById("child-name-text");
 const child_age_input = document.getElementById("child-age-text");
 const child_gender_select = document.getElementById("child-gender-select");
-const checkbox = document.querySelectorAll('input[type=checkbox]');
-const child_interests_input = document.getElementById("child-interests-text");
+const child_interests_table = document.getElementById("child-interests-table");
+
+for (var i = 0; i < interests_activitys.length; i++) {    
+    // https://www.w3schools.com/jsref/met_table_insertrow.asp
+    var row = child_interests_table.insertRow(0);    
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
+    row.innerHTML = `<tr><td>${interests_activitys[i]}</td><td><input type="checkbox" id="${getID(interests_activitys[i])}"></td></tr>`
+    /*
+    // https://stackoverflow.com/questions/24771218/
+    var input = document.createElement('input');
+    input.type = "checkbox";
+    row.
+    */
+}
+//const checkbox = document.querySelectorAll('input[type=checkbox]');
 
 const child_submit_button = document.getElementById('child-submit-button');
 
 // senior-activity components
 const senior_img = document.getElementById('senior-img'); 
-const activity_span = document.getElementById('activity-span'); 
+const activity_content_span = document.getElementById('activity-content-span'); 
 
 // child submit button
 child_submit_button.onclick = () => {
@@ -25,16 +41,16 @@ child_submit_button.onclick = () => {
     console.log(`child age: ${child_age_input.value}`)
     console.log(`child gender: ${child_gender_select.value}`)
     console.log(`child interest:`)
-    for (var i = 0; i < checkbox.length; i++) {        
+
+    for (var i = 0; i < interests_activitys.length; i++) {    
         console.log(`${checkbox[i].id} ${checkbox[i].checked}`)
-    }
-    console.log(`other interest: ${child_interests_input.value}`)
-    console.log(`=====`)
+    }    
+    console.log(`=====childData`)
 
 
     senior_img.src = seionr_img_src
     senior_img.removeAttribute("hidden")
-    activity_span.innerHTML = activity
+    activity_content_span.innerHTML = activity
 }
 
 // senior accept button
@@ -71,3 +87,6 @@ parent_submit_button.onclick = () => {
     console.log(`=====`)
 }
 
+function getID(string) {
+    return string.toLowerCase().replaceAll(' ', '-')
+}
